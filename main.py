57237be1ecbff450baae3e7fb2fea04facda4cb3
nih4t <nihatkky@gmail.com>
2024-09-08 02:47:20 +0200

    data file is improved

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flash Cards")
+window.config(bg=BACKGROUND_COLOR,pady=50,padx=50)
+
+canvas = Canvas(height=526, width=800, highlightthickness=0, bg=BACKGROUND_COLOR)
+image_front = PhotoImage(file="./images/card_front.png")
+canvas.create_image(410, 263, image=image_front)
+canvas.pack()
+
+window.mainloop()
